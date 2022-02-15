81a58723e7fd610200fdf096c943cafe0cbe845b
pk <pradeephellofriend@gmail.com>
2022-02-15 13:54:53 +0530

    style-modified

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,14 +1,20 @@
 import React from 'react'
+import {StylesProvider,createGenerateClassName} from '@material-ui/core/styles'
 import MarketingApp from './components/MarketingApp' 
 import Header from './components/Header'
 import {BrowserRouter} from 'react-router-dom'
+const generateClassName=createGenerateClassName({
+    productionPrefix:'co'
+});
 export default()=>{
     return(
         <BrowserRouter>
+            <StylesProvider generateClassName={generateClassName}>
             <div>
                 <Header/>
                 <MarketingApp/>
             </div>
+            </StylesProvider>
         </BrowserRouter>
     )
 }
